d54135083bc66fca7f9a18f1910e4d8b811db895
serrgeyOkhr <42943550+serrgeyOkhr@users.noreply.github.com>
2018-12-01 15:09:42 +0700

    01.12

--- JQ/les2/lesson 2/js/main.js
+++ JQ/les2/lesson 2/js/main.js
@@ -1,9 +1,35 @@
 /* Взаимодействия с элементами */
 
-$('h2, p') // Первый, второй, третий и т.д.
-$('h2 span') // outer и inner вернет абсолютно все спан лежащие в h2
-$('h2 > span') // parent > child а это вернет только span? который дочка h2
-$('h2 + p') // prev + next выделяется элемент(р) следующий после(h2) но только самый первый
-$('h2 ~ p') // prev ~ next 
+// $('h2, p').css('border', '1px solid #000'); // Первый, второй, третий и т.д.
+// $('h2 span') // outer и inner вернет абсолютно все спан лежащие в h2
+// $('h2 > span') // parent > child а это вернет только span? который дочка h2
+//$('h2 + p').css('border', '2px solid #000'); // prev + next выделяется элемент(р) следующий после(h2) но только самый первый
+// $('h2 ~ p') // prev ~ next 
+// $('.second_ul li:first').css('border', '2px solid #000');
+// $('.second_ul li:last').css('border', '2px solid #000');
+// $('.second_ul li:odd').css('border', '2px solid #000');
+// $('.second_ul li:gt(2)').css('background', 'red');
+//.css('border', '1px solid #000'); 	
+// $('form .from-control').css('border', '1px solid #000');
+// $('button').css({
+//   border: 'solid 1px red',
+//   background: 'yellow'
+// });
+//Найти все radio-элементы из одной группы (где gender это значение атрибута name у искомой группы переключателей)
 
-//.css('border', '2px solid #000'); 	
+$('input[name = gender]').css('border', '1px solid red');
+$('button').click(function (e) { 
+  console.log('Приветствую, пользователь!')
+});
+$('button').dblclick(function (e) { 
+  $(this).addClass('active');
+});
+$('.second_ul li').css('display','none')
+$('h3, .menu').mouseenter(function () { 
+  $('.second_ul li').css('display','block')
+  // $('.second_ul li').css('border','solid 1px red')
+});
+$('h3, .menu').mouseleave(function () { 
+  $('.second_ul li').css('display','none')
+  // $('.second_ul li').css('border','none')
+});
